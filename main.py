37dd2d7cf923e8033955bb5b8e819dab13dce9c1
Lucas Castro <lucasvitor.321lv@gmail.com>
2026-03-17 22:20:16 -0300

    teste

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from config import gravar_audio, audio_para_texto, engine
+from config import gravar_audio, audio_para_texto
 from websocket import create_connection
+from elevenlabs.client import ElevenLabs
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ws = create_connection("ws://localhost:8000/ws")
 
+elevenlabs  = ElevenLabs(api_key=os.getenv("ELEVEN_LABS"))
+
 def falar(texto):
-    engine.stop()
-    engine.say(texto)
-    engine.runAndWait()
+    audio = elevenlabs.text_to_speech.convert(
+        text=texto,
+        voice_id="JBFqnCBsd6RMkjVDRZzb",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+
+    return audio
+
 
 def main():
     while True:
